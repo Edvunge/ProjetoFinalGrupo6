02bff1a889cac4e3c431413b1ee8c506ccb0254c
omaisharu <arievilopedro@gmail.com>
2022-10-07 15:56:55 +0100

    Navbar e Header quase prontas

--- projeto_final/src/components/header/Header.js
+++ projeto_final/src/components/header/Header.js
@@ -1,10 +1,58 @@
 import styles from "./Header.module.css";
+import WorkIcon from "@mui/icons-material/Work";
+import GroupIcon from "@mui/icons-material/Group";
+import PersonIcon from "@mui/icons-material/Person";
+import AssignmentIndIcon from "@mui/icons-material/AssignmentInd";
+import MeetingRoomIcon from "@mui/icons-material/MeetingRoom";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 
 export const Header = () => {
   return (
     <div className={styles.header}>
-      <div className={styles.headerlist}>
-        <div className={styles.headerlistitem}></div>
+      <div className={styles.headerContainer}>
+        <div className={styles.headerList}>
+          <div className={styles.headerListItem}>
+            <AssignmentIndIcon />
+            <span>Freelancers</span>
+          </div>
+          <div className={styles.headerListItem}>
+            <WorkIcon />
+            <span>Empresas</span>
+          </div>
+          <div className={styles.headerListItem}>
+            <GroupIcon />
+            <span>Pequenos Grupos</span>
+          </div>
+          <div className={styles.headerListItem}>
+            <PersonIcon />
+            <span>Individuais</span>
+          </div>
+        </div>
+        <h1 className={styles.headerTitle}>Be with us</h1>
+        <p className={styles.headerDescription}>
+          Encontre locais para trabalhar em seu próximo projeto...
+        </p>
+        <div className={styles.headerSearch}>
+          <div className={styles.headerSearchItem}>
+            <MeetingRoomIcon className={styles.headerIcon} />
+            <input
+              type="text"
+              placeholder="Lisboa..."
+              className={styles.headerSearchInput}
+            ></input>
+          </div>
+          <div className={styles.headerSearchItem}>
+            <CalendarMonthIcon className={styles.headerIcon} />
+            <span className={styles.headerSearchText}>date do date</span>
+          </div>
+          <div className={styles.headerSearchItem}>
+            <PersonIcon className={styles.headerIcon} />
+            <span className={styles.headerSearchText}>2 People</span>
+          </div>
+          <div className={styles.headerSearchItem}>
+            <button className={styles.headerButton}>Search</button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- projeto_final/src/components/navbar/Navbar.js
+++ projeto_final/src/components/navbar/Navbar.js
@@ -1,4 +1,5 @@
 import styles from "./Navbar.module.css";
+import HailIcon from "@mui/icons-material/Hail";
 
 export const Navbar = () => {
   return (
@@ -6,7 +7,7 @@ export const Navbar = () => {
       <div className={styles.navconteiner}>
         <span className={styles.logo}>Hive.Co</span>
         <div className={styles.navitens}>
-          <button className={styles.navbutton}>Registro</button>
+          <button className={styles.navbutton}>Registo</button>
           <button className={styles.navbutton}>Login</button>
         </div>
       </div>
