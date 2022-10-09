99fbb52c39d0ec5a9d9658cb202123f61c9c6151
Pedro Oliveira <arievilopedro@gmail.com>
2022-10-09 14:02:24 +0100

    Iniciando página de pesquisa

--- projeto_final/src/components/list/List.js
+++ projeto_final/src/components/list/List.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import Navbar from "../navbar/Navbar";
 import Header from "../header/Header";
+import styles from "./List.module.css";
 
 export default function List() {
   return (
     <div>
         <Navbar />
         <Header type="list"/>
+        <div className={styles.listContainer}>
+          <div className={styles.listWrapper}>
+            <div className={styles.listSearch}>
+              <h1 className={styles.lsTitle}>Search</h1>
+            </div>
+            <div className={styles.listResult}></div>
+          </div>
+        </div>
     </div>
   )
 }
