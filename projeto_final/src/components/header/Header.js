15efa7fbfd6afe80c7c3522d58fd022346267e87
omaisharu <arievilopedro@gmail.com>
2022-10-10 15:07:41 +0100

    Att

--- projeto_final/src/components/header/Header.js
+++ projeto_final/src/components/header/Header.js
@@ -8,7 +8,6 @@ import { useState } from "react";
 import "react-date-range/dist/styles.css"; // main css file
 import "react-date-range/dist/theme/default.css"; // theme css file
 
-
 export default function Header({ type }) {
   const [districts, setDistricts] = useState("");
   const [openDate, setOpenDate] = useState(false);
@@ -22,20 +21,28 @@ export default function Header({ type }) {
     },
   ]);
 
-
   const handleOption = (operation) => {
-    setQtdPeople((previous) => operation === "i" ? previous + 1 : previous - 1)
-  }
-
+    setQtdPeople((previous) =>
+      operation === "i" ? previous + 1 : previous - 1
+    );
+  };
 
   return (
     <div className={styles.header}>
-      <div className={type === "list" ? styles.headerContainerlistMode : styles.headerContainer}>
-        {type !== "list" &&
+      <div
+        className={
+          type === "list"
+            ? styles.headerContainerlistMode
+            : styles.headerContainer
+        }
+      >
+        {type !== "list" && (
           <>
-            <h1 className={styles.headerTitle}>Be with us</h1>
+            <h1 className={styles.headerTitle}>
+              Cansado de trabalhar em casa?
+            </h1>
             <p className={styles.headerDescription}>
-              Encontre locais para trabalhar em seu próximo projeto...
+              Encontre espaços para trabalhar em seu próximo projeto...
             </p>
             <div className={styles.headerSearch}>
               <div className={styles.headerSearchItem}>
@@ -49,37 +56,66 @@ export default function Header({ type }) {
               </div>
               <div className={styles.headerSearchItem}>
                 <CalendarMonthIcon className={styles.headerIcon} />
-                <span onClick={() => setOpenDate(!openDate)} className={styles.headerSearchText}>{`${format(
-                  date[0].startDate,
+                <span
+                  onClick={() => setOpenDate(!openDate)}
+                  className={styles.headerSearchText}
+                >{`${format(date[0].startDate, "dd/MM/yyyy")} to ${format(
+                  date[0].endDate,
                   "dd/MM/yyyy"
-                )} to ${format(date[0].endDate, "dd/MM/yyyy")}`}</span>
-                {openDate && <DateRange
-                  editableDateInputs={true}
-                  onChange={(item) => setDate([item.selection])}
-                  moveRangeOnFirstSelection={false}
-                  ranges={date}
-                  className={styles.date}
-                  minDate={new Date()}
-                />}
+                )}`}</span>
+                {openDate && (
+                  <DateRange
+                    editableDateInputs={true}
+                    onChange={(item) => setDate([item.selection])}
+                    moveRangeOnFirstSelection={false}
+                    ranges={date}
+                    className={styles.date}
+                    minDate={new Date()}
+                    rangeColors={["#ffbd59", "#231f1f", "#ffbd59"]}
+                  />
+                )}
               </div>
               <div className={styles.headerSearchItem}>
                 <PersonIcon className={styles.headerIcon} />
-                <span onClick={() => setOpenPeople(!openPeople)} className={styles.headerSearchText}>{qtdPeople}</span>
+                <span
+                  onClick={() => setOpenPeople(!openPeople)}
+                  className={styles.headerSearchText}
+                >
+                  {qtdPeople}
+                </span>
               </div>
-              {openPeople && <div className={styles.qtdPeople}>
-                <div className={styles.optionQtdPeople}>
-                  <div className={styles.optionCounter}>
-                    <button disabled={qtdPeople <= 1} className={styles.optionCounterButton} onClick={() => handleOption("d")}>-</button>
-                    <span className={styles.optionCounterNumber}>{qtdPeople}</span>
-                    <button disabled={qtdPeople >= 12} className={styles.optionCounterButton} onClick={() => handleOption("i")}>+</button>
+              {openPeople && (
+                <div className={styles.qtdPeople}>
+                  <div className={styles.optionQtdPeople}>
+                    <div className={styles.optionCounter}>
+                      <button
+                        disabled={qtdPeople <= 1}
+                        className={styles.optionCounterButton}
+                        onClick={() => handleOption("d")}
+                      >
+                        -
+                      </button>
+                      <span className={styles.optionCounterNumber}>
+                        {qtdPeople}
+                      </span>
+                      <button
+                        disabled={qtdPeople >= 12}
+                        className={styles.optionCounterButton}
+                        onClick={() => handleOption("i")}
+                      >
+                        +
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>}
+              )}
               <div className={styles.headerSearchItem}>
                 <button className={styles.headerButton}>Search</button>
               </div>
-            </div></>}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
-};
+}
