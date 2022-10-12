36144fa346c9d69e0d69cc9e62a5177a802b0aa1
Edvunge <costa07buda@gmail.com>
2022-10-12 10:39:40 +0100

    created PUT

--- projeto_final/pages/api/places.js
+++ projeto_final/pages/api/places.js
@@ -16,6 +16,22 @@ export default async function (req, res) {
             console.log("SAVED")
             res.status(200).json(newPlace)
         }
+    }
 
+    //UPDATE
+    if (req.method === "PUT") {
+        await connect()
+       /* if (req.body) {
+           
+            const newPlace = new Places(req.body)
+            await newPlace.save()
+            res.status(200).json(newPlace)   
+        }*/
+        try {
+            const updatedPlaces = await Places.findByIdAndUpdate(req.params.id, {$set: req.body})
+            res.status(200).json(updatedPlaces);
+        } catch (err){
+            res.status(500).json(err);
+        }
     }
 } 
---
+++ projeto_final/pages/api/places/[id].js
@@ -0,0 +1,24 @@
+import { connect } from "../database"
+import Places from "../../../src/models/Places"
+
+export default async function (req, res) {
+  
+
+   
+    //UPDATE
+    if (req.method === "PUT") {
+        await connect()
+       /* if (req.body) {
+           
+            const newPlace = new Places(req.body)
+            await newPlace.save()
+            res.status(200).json(newPlace)   
+        }*/
+        try {
+            const updatedPlaces = await Places.findByIdAndUpdate(req.query.id, {$set: req.body})
+            res.status(200).json(updatedPlaces);
+        } catch (err){
+            res.status(500).json(err);
+        }
+    }
+} 
