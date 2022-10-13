8d8a6e3522c80f86cd5ffe2cde2f571e06182ba9
Compl3x13 <steamds13@gmail.com>
2022-10-13 15:52:17 +0100

    3rd Alteração do login e resgitar

--- projeto_final/src/components/registo/resgisto.js
+++ projeto_final/src/components/registo/resgisto.js
@@ -1,52 +1,56 @@
 import React from 'react'
 import styles from "./Registo.module.css";
 import Link from "next/link";
+import Image from "next/image"
+import { style } from '@mui/system';
 
 
 
-
-export default function Registo() {
-
-    const signup = () => {
-        fetch()
-    }
+export default function LoginForm() {
     return (
         <div className={styles.divContainer}>
             <div className={styles.divLogin}>
-                <h2>Registar</h2>
-                <span>
-                    Nome:
-                    <br /><input placeholder="Nome" type="text"></input>
-                </span>
-                <br />
-                <span>
-                    Email:
-                    <br /><input placeholder="example@example.com" type="text"></input>
-                </span>
 
-                <br />
-                <span>
-                    Password:
-                    <br /><input placeholder="**********" type="password"></input>
-                </span>
-                <br />
-                <span>
-                    Confirmar Password:
-                    <br /><input placeholder="**********" type="password"></input>
-                </span>
-                <br />
-                <span>
-                    Telemóvel:
-                    <br /><input placeholder="912 345 678" type="tel"></input>
-                </span>
-                <div className={styles.divButtons}>
-                    <Link href="/">
-                    <button className={styles.button}>Registar</button>
-                    </Link>
+                <div className={styles.divDireita}>
 
+                    <h2>Registar</h2>
+                    <span>
+                        Nome:
+                        <br /><input placeholder="Nome" type="text"></input>
+                    </span>
+                    <br />
+                    <span>
+                        Email:<br />
+                        <br /><input placeholder="example@example.com" type="text"></input>
+                    </span>
+                    <br />
+                    <span>
+                        Password:<br />
+                        <br /><input placeholder="*********" type="password"></input>
+                    </span>
+                    <br />
+                    <span>
+                        Confirmar Password:<br />
+                        <br /><input placeholder="**********" type="password"></input>
+                    </span>
+                    <br />
+                    <span>
+                        Telemóvel:
+                        <br /><input placeholder="912 345 678" type="tel"></input>
+                    </span>
+                    <div className={styles.divButtons}>
+                        <Link href="/"><button className={styles.button}>Registar</button></Link>
+                    </div>
 
                 </div>
+                <div className={styles.divEsquerda}>
+                    <div className={styles.divEsquerdaPequena}>
+                    </div>
+
+                    <div className={styles.divEsquerdaPequenaDireita}><span className={styles.hive}>Hive.</span><span className={styles.co}> co</span>
+                    </div>
+                </div>
             </div>
-        </div>
+        </div >
     )
 }
