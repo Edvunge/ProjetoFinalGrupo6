1cb19ed2eaea749a094fb4658886922a56f4b2e5
Edvaldo vunge <53275378+Edvunge@users.noreply.github.com>
2022-10-14 11:37:00 +0100

    test

--- projeto_final/pages/test.js
+++ projeto_final/pages/test.js
@@ -1,46 +1,145 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react'
-import styles from "../styles/test.module.css"
-
-
-
-export default function sitios() {
-    if (!typeof window === 'undefined') return <div></div>
-
-    return (
-        <div className={styles.wrapper}>
-            <div style={{ height: "1240px", width: "100%" }}></div>
-            <Item type="right">
-                ola
-            </Item>
-            <div style={{ height: "40px", width: "100%" }}></div>
-            <Item type="left">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis neque at tellus porttitor consequat scelerisque in arcu. Aliquam sit amet metus diam. Sed fringilla diam et aliquam viverra. Proin dignissim mattis fermentum. Sed sit amet mauris cursus, convallis neque vitae, dictum nunc. Sed in nisl mollis, vestibulum nulla vitae, laoreet odio. In dictum posuere finibus. Pellentesque dignissim bibendum facilisis. Aliquam eu sapien lectus.
-            </Item>
-            <div style={{ height: "1240px", width: "100%" }}></div>
-        </div >
-    );
-}
-
-const Item = ({ children, type }) => {
-    const ref = useRef(null)
-    const [isInViewPort, setInViewPort] = useState(false)
-    useEffect(() => {
-        const handleScroll = () => {
-            const box = ref.current.getBoundingClientRect()
-            setInViewPort(box.bottom <= window.innerHeight)
-        }
-        document.addEventListener("scroll", handleScroll)
-        return () => document.removeEventListener("scroll", handleScroll)
-    }, [ref])
-    return (
-        <div ref={ref}
-            style={{ transform: `translateX(${type === "right" ? "+" : "-"}${isInViewPort ? 0 : 3000}px)` }}
-            className={
-                [styles.animated]
-                    .concat(isInViewPort ? styles.active : null)
-                    .join(" ")
-            } >
-            {children}
-        </div >
-    )
-}
+[
+  {
+    "city": "Stagecoach",
+    "desc": "Duis mollit consequat aliqua quis excepteur mollit id cillum occaecat.",
+    "name": "Zoarere",
+    "photos": [
+      "bcca66e4-2218-4bd0-8320-26f1a4b4b30b"
+    ],
+    "rating": [
+      1,
+      3
+    ],
+    "address": "Cranberry Street",
+    "roomsNr": 7
+  },
+  {
+    "city": "Hanover",
+    "desc": "Sunt dolore deserunt veniam in qui laboris laboris.",
+    "name": "Valpreal",
+    "photos": [
+      "ef90eba3-4617-4395-b075-875915f04441",
+      "ddc15193-a681-44ca-aa21-bb470154124e"
+    ],
+    "rating": [
+      10,
+      1,
+      8,
+      5,
+      8,
+      6,
+      10,
+      1,
+      2,
+      5
+    ],
+    "address": "Bragg Street",
+    "roomsNr": 10
+  },
+  {
+    "city": "Robinson",
+    "desc": "Consectetur enim aute enim Lorem irure sint id Lorem dolore non eu id exercitation.",
+    "name": "Joviold",
+    "photos": [
+      "ce6ca193-12e8-46eb-80f2-1639da99ea62",
+      "a56f20f9-7b07-4675-904c-9b41b7b9d3cd",
+      "d87bba00-fdf8-4a5f-8721-aa19ca73cc06",
+      "ec615266-db61-48ad-ac82-8f6f905ad4b9"
+    ],
+    "rating": [
+      6
+    ],
+    "address": "Bassett Avenue",
+    "roomsNr": 6
+  },
+  {
+    "city": "Morningside",
+    "desc": "In nulla velit excepteur officia laborum commodo do veniam est ad do eiusmod ea commodo.",
+    "name": "Glukgluk",
+    "photos": [
+      "a33544d1-4ee0-40d4-a984-ddec87b2d5dd"
+    ],
+    "rating": [
+      1,
+      7,
+      7,
+      6,
+      10,
+      2
+    ],
+    "address": "Denton Place",
+    "roomsNr": 8
+  },
+  {
+    "city": "Crown",
+    "desc": "Irure labore fugiat elit do sit ipsum labore.",
+    "name": "Fuelworks",
+    "photos": [
+      "427bfdc7-3aa3-41f5-9dda-858610b4a4e3"
+    ],
+    "rating": [
+      2,
+      3,
+      7,
+      4,
+      8,
+      7,
+      10,
+      10
+    ],
+    "address": "Greene Avenue",
+    "roomsNr": 1
+  },
+  {
+    "city": "Courtland",
+    "desc": "Quis ea sit est mollit.",
+    "name": "Waab",
+    "photos": [
+      "afe4a4f4-1bf3-4beb-b92a-efe6ec8960bd"
+    ],
+    "rating": [
+      1
+    ],
+    "address": "Degraw Street",
+    "roomsNr": 2
+  },
+  {
+    "city": "Lemoyne",
+    "desc": "Qui deserunt cupidatat laborum quis.",
+    "name": "Aclima",
+    "photos": [
+      "d52afd45-9dee-4d12-8399-db2c0e880c92",
+      "8502f8f5-423b-4a1a-a21e-170f578c1bb0",
+      "6b8a2cc7-6e96-461f-a8c7-480e3b809c86"
+    ],
+    "rating": [
+      8,
+      8,
+      8,
+      3,
+      10,
+      6,
+      2
+    ],
+    "address": "Dewey Place",
+    "roomsNr": 2
+  },
+  {
+    "city": "Curtice",
+    "desc": "Est laboris Lorem culpa aliquip.",
+    "name": "Softmicro",
+    "photos": [
+      "6c025030-c43c-4b2d-9836-314f91d06638",
+      "88322c84-a382-42c8-a3bd-345115980949",
+      "ec0dc73c-e615-40dc-83b7-23f5d6e11b0a",
+      "42933464-341a-4d16-9167-38f154054a05"
+    ],
+    "rating": [
+      4,
+      7,
+      8
+    ],
+    "address": "Delmonico Place",
+    "roomsNr": 7
+  }
+]
